1ff8e166baf0fc1ebab12c1d8f0ebddcf1121dd3
MayouKurayami <mkw801021@gmail.com>
2020-02-14 12:35:01 -0800

    Fix typo in comment

--- KK_HAutoSets/VRHScene_Patches.cs
+++ KK_HAutoSets/VRHScene_Patches.cs
@@ -17,7 +17,7 @@ namespace KK_HAutoSets
 
 		private static MethodInfo TargetMethod()
 		{
-			//this is should hook to a method that loads as late as possible in the loading phase
+			//This should hook to a method that loads as late as possible in the loading phase
 			//Hooking method "MapSameObjectDisable" because: "Something that happens at the end of H scene loading, good enough place to hook" - DeathWeasel1337/Anon11
 			//https://github.com/DeathWeasel1337/KK_Plugins/blob/master/KK_EyeShaking/KK.EyeShaking.Hooks.cs#L20
 			return Type.GetType("VRHScene,Assembly-CSharp.dll").GetMethod("MapSameObjectDisable", AccessTools.all);
