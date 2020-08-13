d172f364c5973916dbc4db4634e5fcb676685709
MayouKurayami <mkw801021@gmail.com>
2020-08-13 14:19:05 -0700

    Increment version number

--- KK_HSceneOptions/HSceneOptions.cs
+++ KK_HSceneOptions/HSceneOptions.cs
@@ -25,7 +25,7 @@ namespace KK_HSceneOptions
 		public const string GUID = "MK.KK_HSceneOptions";
 		public const string PluginName = "HSceneOptions";
 		public const string AssembName = "KK_HSceneOptions";
-		public const string Version = "3.0.0";
+		public const string Version = "3.0.1";
 
 		internal static bool isVR;
 		internal static bool isDarkness;
